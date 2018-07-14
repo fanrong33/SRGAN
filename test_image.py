590deb04b098547fb27aa553a166b514eccefb83
fanrong33 <fanrong33@qq.com>
2018-07-14 17:40:40 +0800

    update

---
+++ test_image.py
@@ -0,0 +1,52 @@
+# encoding: utf-8
+"""
+"""
+
+import argparse
+import time
+
+import torch
+from PIL import Image
+from torch.autograd import Variable
+from torchvision.transforms import ToTensor, ToPILImage
+
+from model import Generator
+
+parser = argparse.ArgumentParser(description='Test Single Image')
+parser.add_argument('--upscale_factor', default=4, type=int, help='super resolution upscale factor')
+parser.add_argument('--test_mode', default='GPU', type=str, choices=['GPU', 'CPU'], help='using GPU or CPU')
+parser.add_argument('--image_name', type=str, help='test low resolution image name')
+parser.add_argument('--model_name', default='netG_epoch_4_100.pth', type=str, help='generator model epoch name')
+opt = parser.parse_args()
+
+CUDA = torch.cuda.is_available()
+
+
+UPSCALE_FACTOR = opt.upscale_factor
+TEST_MODE = True if opt.test_mode == 'GPU' else False
+IMAGE_NAME = opt.image_name
+MODEL_NAME = opt.model_name
+
+model = Generator(UPSCALE_FACTOR).eval()
+if CUDA:
+    model.cuda()
+    model.load_state_dict(torch.load('epochs/' + MODEL_NAME))
+else:
+    model.load_state_dict(torch.load('epochs/' + MODEL_NAME, map_location=lambda storage, loc: storage))
+
+
+image = Image.open(IMAGE_NAME)
+image = Variable(ToTensor()(image), volatile=True)
+if CUDA:
+    image = image.cuda()
+image = image[:3].unsqueeze(0)
+
+start = time.clock()
+out = model(image)
+elapsed = (time.clock() - start)
+print('cost' + str(elapsed) + 's')
+
+out_img = ToPILImage()(out[0].data.cpu())
+out_img.save('out_srf_' + str(UPSCALE_FACTOR) + '_' + IMAGE_NAME)
+
+
